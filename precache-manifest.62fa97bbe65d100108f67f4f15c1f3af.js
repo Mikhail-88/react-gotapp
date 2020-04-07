3dcb40a11d568d990e7af017dbd4ea54ba588cf3
Mikhail <mikhail.sapuzhak@gmail.com>
2020-04-07 15:15:42 +0300

    Updates

---
+++ precache-manifest.62fa97bbe65d100108f67f4f15c1f3af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "844c9361a2f8808ab1e3",
+    "url": "/react-gotapp/static/css/main.3af00e8e.chunk.css"
+  },
+  {
+    "revision": "844c9361a2f8808ab1e3",
+    "url": "/react-gotapp/static/js/main.844c9361.chunk.js"
+  },
+  {
+    "revision": "ce314489de20bf327487",
+    "url": "/react-gotapp/static/css/1.17a1ef20.chunk.css"
+  },
+  {
+    "revision": "ce314489de20bf327487",
+    "url": "/react-gotapp/static/js/1.ce314489.chunk.js"
+  },
+  {
+    "revision": "5c25e3a71a5217ea02b3",
+    "url": "/react-gotapp/static/js/runtime~main.5c25e3a7.js"
+  },
+  {
+    "revision": "3c0106988b43910f4f1c191b34a7d0ce",
+    "url": "/react-gotapp/static/media/got.3c010698.jpeg"
+  },
+  {
+    "revision": "254be39132eee4f1a72f9a248f894460",
+    "url": "/react-gotapp/index.html"
+  }
+];
